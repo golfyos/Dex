da9b91876999d35f9149b0c95cd95af8078d3195
golf <golf_cat1@hotmail.com>
2016-09-04 19:57:36 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# My Pokedex
+
+## Feature
+
+- Display pokemon I have caught
+- Display name of pokemon when click on picture
+- Display type of pokemon when click on picture
+- It should automatically display pokemon picture
+
+- Can add new pokemon to the list
+- Display a hint while I am typing a pokemon name
+- Can store IV of each pokemon
---
+++ info.js
@@ -0,0 +1,53 @@
+function p1(){
+    alert("Pokemon Name: DRAGONITE");
+}
+function p2(){
+    alert("Pokemon Name: DODRIO");
+}
+function p3(){
+    alert("Pokemon Name: EEVEE");
+}
+function p4(){
+    alert("Pokemon Name: PSYDUCK");
+}
+function p5(){
+    alert("Pokemon Name: GOLDUCK");
+}
+function p6(){
+    alert("Pokemon Name: MAGIKARP");
+}
+function p7(){
+    alert("Pokemon Name: GYARADOS");
+}
+function p8(){
+    alert("Pokemon Name: JYNX");
+}
+function p9(){
+    alert("Pokemon Name: MEOWTH");
+}
+function p10(){
+    alert("Pokemon Name: PERSIAN");
+}
+function p11(){
+    alert("Pokemon Name: NINETALES");
+}
+function p12(){
+    alert("Pokemon Name: ONIX");
+}
+function p13(){
+    alert("Pokemon Name: PIKACHU");
+}
+function p14(){
+    alert("Pokemon Name: RATTATA");
+}
+function p15(){
+    alert("Pokemon Name: SLOWPOKE");
+    alert("IT'S RUNAWAY FROM YOU !!");
+}
+function p16(){
+    alert("Pokemon Name: SLOWBRO");
+}
+function p17(){
+    alert("Pokemon Name: SNORLAX");
+    alert("IT'S RUNAWAY FROM YOU !!");
+}
